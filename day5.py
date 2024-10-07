7a90f5e1c6a42f7f253ee61686b111a59aa6e773
Dipan Kharel <dipankharel@gmail.com>
2024-10-07 23:17:06 +0545

    Day5 solution

---
+++ day5.py
@@ -0,0 +1,29 @@
+#min stack
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        for i in range(len(self.stack)-1, -1, -1):
+            self.min = self.stack[i]
+            if self.min > self.stack[i-1]:
+                self.min = self.stack[i-1]
+        return self.min
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
